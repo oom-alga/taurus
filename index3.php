f9dd0007fc6fb435105ca6706c252396240c30fe
oom-alga <sosial.alga@gmail.com>
2018-07-11 06:27:20 +0700

    index2 done dan ini mulai dari index 3

---
+++ index3.php
@@ -0,0 +1,74 @@
+<?php include "header.php"; ?>
+
+<div class="index3 img-flex">
+   <img src="images/portfolio/adult-arms-crossed-bow-tie-1702.jpg" alt="header">
+</div>
+<div class="art2">
+  <div class="container">
+    <div class="row about2 justify-content-center text-center">
+      <div class="col-md-5 img-flex">
+        <img src="images/portfolio/adult-arms-crossed-bow-tie-1702.jpg" alt="header">
+      </div>
+      <div class="col-md-5 article">
+        <i>Sambel Pecel</i>
+        <p>
+          For example, here are two grid layouts that apply to every device and viewport
+        </p>
+        <button type="button" class="btn btn-outline-secondary">Secondary</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+ <!-- Portfolio -->
+ <div id="portfolio" class="portfolio3">
+    <div class="container">
+      <div class="row article-me">
+        <div class="portfolio-item satu">
+          <a class="portfolio-link">
+            <div class="caption">
+              <div class="caption-content">
+                <h3>The Shape of Design</h3>
+                <h4>Branding/Graphic1</h4>
+              </div>
+            </div>
+            <img src="images/portfolio/adult-arms-crossed-bow-tie-1702.jpg" class="img-responsive" alt=""> 
+          </a>
+        </div>
+        <div class="portfolio-item satu">
+          <a class="portfolio-link">
+            <div class="caption">
+              <div class="caption-content">
+                <h3>The Shape of Design</h3>
+                <h4>Branding/Graphic1</h4>
+              </div>
+            </div>
+            <img src="images/portfolio/adult-arms-crossed-bow-tie-1702.jpg" class="img-responsive" alt=""> 
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+<!-- Portfolio end -->
+
+<div id="site-social">
+  <div class="site-social inner no-padding text-center">
+    <div class="social">
+      <a href="#" target="_blank" class="social">
+        <i class="fab fa-facebook-square"></i>
+      </a>
+      <a href="#" target="_blank" class="social">
+        <i class="fab fa-twitter-square"></i>
+      </a>
+    </div>
+    <div class="address">
+      <p>Yogyakarta, Demangan depok sleman</p>
+      <p>1234 Beo, <span class="bold colored">Nusantara</span></p>
+      <a href="#top" class="scroll top-button">
+        <i class="fas fa-angle-double-up"></i>
+      </a>
+    </div>
+  </div>
+</div>
+<?php include "footer.php"; ?>
